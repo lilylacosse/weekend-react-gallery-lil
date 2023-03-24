308552a5934f1d8cce994d0e6cc88f02952585c5
Lily LaCosse <lilylacosse@gmail.com>
2023-03-24 15:52:03 -0500

    updated gallery.data.js

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,35 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: "public/images/goat_small.jpg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: "public/images/crosscountryski.JPG",
+    description: "Jean-Luc getting ready to cross-country ski.",
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: "public/images/newcar.JPG",
+    description:
+      "Bought a cute blue Subaru, because buying a used car was too expensive!",
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: "public/images/playinginsnow.JPG",
+    description: "JL and I playing in the snow during a Breckenridge snow day.",
+    likes: 0,
+  },
+  {
+    id: 5,
+    path: "public/images/southshore.JPG",
+    description: "JL and I on the south shore of Lake Superior.",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
