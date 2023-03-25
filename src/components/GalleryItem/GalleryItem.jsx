e0f72d6c80368ed9617350a6bb688846daea261c
Lily LaCosse <lilylacosse@gmail.com>
2023-03-25 11:54:40 -0500

    GALLERYITEM COMPONENT WORKS!

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -3,13 +3,14 @@ function GalleryItem({ listItem }) {
         "this is listItem:", listItem
     );
     return (
-        <></>
-        // < div key={listItem.id} >
-        //     <img src={listItem.path} width='100' height='100' />
-        //     <div>{listItem.description}</div>
-        //     <button>Like!</button>
-        //     <div>{listItem.likes}</div>
-        // </div>
+        <>
+            < div key={listItem.id} >
+                <img src={listItem.path} width='100' height='100' />
+                <div>{listItem.description}</div>
+                <button>Like!</button>
+                <div>{listItem.likes}</div>
+            </div>
+        </>
     )
 }
 
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -7,15 +7,8 @@ function GalleryList({ galleryList }) {
             <h2>Lily's Photo Gallery</h2>
 
             {galleryList.map((listItem) => (
-                < div key={listItem.id} >
-                    <img src={listItem.path} width='100' height='100' />
-                    <div>{listItem.description}</div>
-                    <button>Like!</button>
-                    <div>{listItem.likes}</div>
-                </div>
-            )
-                // <GalleryItem listItem={listItem} />
-            )
+                <GalleryItem listItem={listItem} />
+            ))
             }
 
         </div >
