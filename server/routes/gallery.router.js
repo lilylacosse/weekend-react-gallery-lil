34547e2108fa446d278e56e3b7bdfdb549d5dd29
Lily LaCosse <lilylacosse@gmail.com>
2023-03-25 17:30:16 -0500

    server get and put are now running through the database

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -7,19 +7,35 @@ const pool = require("../modules/pool.js");
 
 // PUT Route
 router.put("/like/:id", (req, res) => {
-  console.log(req.params);
-  const galleryId = req.params.id;
-  for (const galleryItem of galleryItems) {
-    if (galleryItem.id == galleryId) {
-      galleryItem.likes += 1;
-    }
-  }
-  res.sendStatus(200);
+  //   console.log(req.params.id, req.body);
+  let galleryId = req.params.id;
+  let likeCount = req.body.likeCount;
+
+  likeCount++;
+  //   console.log(likeCount);
+
+  const sqlQuery = `UPDATE photos SET likes=$1 WHERE id=$2;`;
+  pool
+    .query(sqlQuery, [likeCount, galleryId])
+    .then((response) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+    });
 }); // END PUT Route
 
 // GET Route
 router.get("/", (req, res) => {
-  res.send(galleryItems);
+  const sqlQuery = `SELECT * FROM photos ORDER BY id, id DESC;`;
+  pool
+    .query(sqlQuery)
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+    });
 }); // END GET Route
 
 module.exports = router;
